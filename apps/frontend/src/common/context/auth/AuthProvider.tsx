bc974f1869d0fffbbcdcdbf7987effc43cdc8242
Jeff Nielsen <jefftoddnielsen@gmail.com>
2026-02-07 12:48:59 +1100

    Removed duplicate toke store

--- apps/frontend/src/common/context/auth/AuthProvider.tsx
+++ apps/frontend/src/common/context/auth/AuthProvider.tsx
@@ -1,6 +1,5 @@
-import { useEffect, useState, type FC, type ReactNode } from "react";
+import { useState, type FC, type ReactNode } from "react";
 import { AuthContext, type UserType } from "./AuthContext";
-import { useTokenManager } from "../../hooks/useTokenManager";
 
 type AuthProviderType = {
   children: ReactNode;
@@ -9,20 +8,6 @@ type AuthProviderType = {
 export const AuthProvider: FC<AuthProviderType> = ({ children }) => {
   const [user, setUser] = useState<UserType>({ email: null, name: null });
   const [token, setToken] = useState<string | null>(null);
-
-  const { getToken } = useTokenManager();
-
-  useEffect(() => {
-    const initAuth = async () => {
-      const storedToken = await getToken();
-
-      if (storedToken) {
-        setToken(await getToken());
-      }
-    }
-
-    initAuth();
-  }, [getToken]);
 
   return (
     <AuthContext.Provider value={{ user, setUser, token, setToken }}>
