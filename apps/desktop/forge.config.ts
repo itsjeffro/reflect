5374231d867d652703e32c26166b366e45b726bb
Jeff Nielsen <jefftoddnielsen@gmail.com>
2026-02-11 20:55:22 +1100

    Added the following - hooks to forge confic for genearating assets - Use custom protocol in main process

--- apps/desktop/forge.config.ts
+++ apps/desktop/forge.config.ts
@@ -7,13 +7,22 @@ import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-nati
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
 import { FuseV1Options, FuseVersion } from '@electron/fuses';
-
+import { spawn, exec, ChildProcess } from 'child_process';
 import { mainConfig } from './webpack.main.config';
 import { rendererConfig } from './webpack.renderer.config';
+import { promisify } from 'util';
+import path, { join } from 'path';
+import { cp } from 'fs/promises';
+
+const FRONTEND_PATH = '../frontend';
+const execAsync = promisify(exec);
 
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    extraResource: [
+      join(__dirname, 'renderer')
+    ],
   },
   rebuildConfig: {},
   makers: [
@@ -52,6 +61,64 @@ const config: ForgeConfig = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+  hooks: {
+    preStart: async () => {
+      const isDev = process.env.VITE_SERVER;
+      
+      if (!isDev) {
+        console.log(`Skipping preStart`);
+        return;
+      }
+
+      console.log(`Starting up server on platform: ${process.platform}`);
+
+      let viteProcess: ChildProcess | null = null;
+
+      const startServer = new Promise<void>((resolve, reject) => {
+        const viteProjectDir = path.resolve(__dirname, FRONTEND_PATH);
+
+        viteProcess = spawn('pnpm', ['dev', '--port 5174'], {
+          cwd: viteProjectDir,
+          shell: true,
+          stdio: 'pipe',
+          detached: process.platform === 'win32' ? false : true,
+        });
+
+        viteProcess.stdout?.on('data', (data) => {
+          const output = data.toString().trim();
+
+          if (output.includes('ready in') || output.includes('Local:')) {
+            resolve();
+          }
+        });
+
+        process.on('exit', () => viteProcess?.kill());
+
+        setTimeout(() => {
+          reject(new Error(' Starting up server has timed out!'));
+        }, 30000);
+      });
+
+      await startServer;
+    },
+    generateAssets: async () => {
+      const isDev = process.env.VITE_SERVER;
+
+      if (isDev) {
+        console.log(`Skipping generateAssets hook`);
+        return;
+      }
+
+      console.log(`Running generateAssets hook`);
+
+      await execAsync('cd ../frontend && pnpm build');
+
+      const src = path.resolve(__dirname, FRONTEND_PATH, 'dist');
+      const dest = path.resolve(__dirname, 'renderer');
+
+      await cp(src, dest, { recursive: true, force: true });
+    }
+  }
 };
 
 export default config;
