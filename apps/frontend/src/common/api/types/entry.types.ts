5d7b7886c10d5e06cfa4ecd3d438718c4966d5cd
Jeff Nielsen <jefftoddnielsen@gmail.com>
2026-02-19 18:41:47 +1100

    Added tags property

--- apps/frontend/src/common/api/types/entry.types.ts
+++ apps/frontend/src/common/api/types/entry.types.ts
@@ -1,3 +1,5 @@
+import type { TagModel } from "./tag.types";
+
 export type EntryRequest = {
   title?: string | null;
   content?: string | null;
@@ -9,6 +11,7 @@ export type EntryModel = {
   id: number;
   title: string | null;
   content: string | null;
+  tags: TagModel[];
   published_at: string;
   created_at: string | null;
   updated_at: string | null;
